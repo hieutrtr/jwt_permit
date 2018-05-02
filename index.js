a68944dc632178695cb326c996346edab15d18c8
Hieu Tran <hieutt@adasiaholdings.com>
2018-05-02 08:05:36 +0700

    handle by errinstance

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ module.exports = function permit(...allowed) {
    const token = req.headers.authorization.split("bearer ")[1] || req.query.access_token || req.body.access_token;
    jwt.verify(token, secretKey, (err, decoded) => {
        if(err) {
-         return res.status(401).json({
-             message: 'invalid token',
-             data:err
-           }).end();
+         next({
+              message: 'invalid token',
+              data:err
+            })
        } else {
          var rights = decoded.data.role ? Object.keys(decoded.data.role).reduce((rights,key) => {
            if(isAllowed(key)) {
@@ -22,9 +22,9 @@ module.exports = function permit(...allowed) {
          if (rights && rights.length > 0) {
            next();
          } else {
-           return res.status(403).json({
+           next({
              message: 'Forbidden'
-           }).end();
+           })
          }
        }
     })
